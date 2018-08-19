2e97dddd166bb9de45d9d270f7ecd487e9c8620f
Avnish <avnish.akt@gmail.com>
2018-08-19 16:51:38 +0530

    Update_Home9

--- app/src/main/java/sweet/home/pinerria1/Utils/Api.java
+++ app/src/main/java/sweet/home/pinerria1/Utils/Api.java
@@ -13,14 +13,15 @@ public class Api {
     public static String food="http://35.184.93.23:3000/api/food-menu/mobile";
     public static String parent="http://35.184.93.23:3000/api/users/parent/me";
     public static String classApi="http://35.184.93.23:3000/api/class";
-    public static String activities="http://35.184.93.23:3000/api/activities";
+    public static String activities="http://35.184.93.23:3000/api/activities/byclass/";
     public static String Registration="http://35.184.93.23:3000/api/users/parent/register/child";
     public static String PostMsg="http://35.184.93.23:3000/api/message";
     public static String Calender="http://35.184.93.23:3000/api/calendar";
 //    public static String Remark="http://35.184.93.23:3000/api/remarks/bystudentid/";
     public static String Remark="http://35.184.93.23:3000/api/remarksandassessments/bystudentid/remarks/";
     public static String Notification="http://35.184.93.23:3000/api/notification";
-    public static String userroles="http://35.184.93.23:3000/api/userroles";
+//    public static String userroles="http://35.184.93.23:3000/api/userroles";
+    public static String userroles="http://35.184.93.23:3000/api/users/userroles";
     public static String DeleteMsg="http://35.184.93.23:3000/api/message";
     public static String SchoolManuals="http://35.184.93.23:3000/assets/pdf/SchoolManuals.pdf";
     public static String WeeklyPlan="http://35.184.93.23:3000/api/weekly-plan/getmonthandweekbydata/";
