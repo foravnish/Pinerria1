77dfb89fe809752a46120a3fcfb7e5f4ced3b28e
Danny <dinesh.kumar@sntinfotech.com>
2018-10-03 16:57:17 +0530

    Update310

--- app/src/main/java/sweet/home/homesweethome/Fragment/ActivityImage.java
+++ app/src/main/java/sweet/home/homesweethome/Fragment/ActivityImage.java
@@ -1,19 +1,42 @@
 package sweet.home.homesweethome.Fragment;
 
 
+import android.app.Dialog;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.squareup.okhttp.internal.Util;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import sweet.home.homesweethome.R;
 import sweet.home.homesweethome.Utils.AppController;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,7 +50,8 @@ public class ActivityImage extends Fragment {
 
     TextView downalod,title,desc,back;
     NetworkImageView network;
-
+    Dialog dialog;
+    private AsyncTask mMyTask;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -41,6 +65,11 @@ public class ActivityImage extends Fragment {
         downalod=view.findViewById(R.id.downalod);
         network=view.findViewById(R.id.network);
         back=view.findViewById(R.id.back);
+
+        dialog=new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setCancelable(false);
 
         ImageLoader imageLoader = AppController.getInstance().getImageLoader();
         network.setImageUrl(getArguments().getString("image"), imageLoader);
@@ -56,6 +85,8 @@ public class ActivityImage extends Fragment {
             }
         });
 
+        mMyTask = new DownloadTask().execute(stringToURL(getArguments().getString("image")));
+
         downalod.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,4 +96,151 @@ public class ActivityImage extends Fragment {
         return view;
     }
 
+
+
+
+//
+    private class DownloadTask extends AsyncTask<URL,Void,Bitmap> {
+        // Before the tasks execution
+        protected void onPreExecute(){
+            // Display the progress dialog on async task start
+//            mProgressDialog.show();
+            sweet.home.homesweethome.Utils.Util.showPgDialog(dialog);
+        }
+
+        // Do the task in background/non UI thread
+        protected Bitmap doInBackground(URL...urls){
+            URL url = urls[0];
+            HttpURLConnection connection = null;
+
+            try{
+                // Initialize a new http url connection
+                connection = (HttpURLConnection) url.openConnection();
+
+                // Connect the http url connection
+                connection.connect();
+
+                // Get the input stream from http url connection
+                InputStream inputStream = connection.getInputStream();
+
+                /*
+                    BufferedInputStream
+                        A BufferedInputStream adds functionality to another input stream-namely,
+                        the ability to buffer the input and to support the mark and reset methods.
+                */
+                /*
+                    BufferedInputStream(InputStream in)
+                        Creates a BufferedInputStream and saves its argument,
+                        the input stream in, for later use.
+                */
+                // Initialize a new BufferedInputStream from InputStream
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+                /*
+                    decodeStream
+                        Bitmap decodeStream (InputStream is)
+                            Decode an input stream into a bitmap. If the input stream is null, or
+                            cannot be used to decode a bitmap, the function returns null. The stream's
+                            position will be where ever it was after the encoded data was read.
+
+                        Parameters
+                            is InputStream : The input stream that holds the raw data
+                                              to be decoded into a bitmap.
+                        Returns
+                            Bitmap : The decoded bitmap, or null if the image data could not be decoded.
+                */
+                // Convert BufferedInputStream to Bitmap object
+                Bitmap bmp = BitmapFactory.decodeStream(bufferedInputStream);
+
+                // Return the downloaded bitmap
+                return bmp;
+
+            }catch(IOException e){
+                e.printStackTrace();
+            }finally{
+                // Disconnect the http url connection
+                connection.disconnect();
+            }
+            return null;
+        }
+
+        // When all async task done
+        protected void onPostExecute(Bitmap result){
+            // Hide the progress dialog
+//            mProgressDialog.dismiss();
+            sweet.home.homesweethome.Utils.Util.cancelPgDialog(dialog);
+
+            if(result!=null){
+                // Display the downloaded image into ImageView
+               // mImageView.setImageBitmap(result);
+
+                // Save bitmap to internal storage
+                Uri imageInternalUri = saveImageToInternalStorage(result);
+                Log.d("sgfsdgdfgdfgdf", String.valueOf(imageInternalUri));
+                // Set the ImageView image from internal storage
+               // mImageViewInternal.setImageURI(imageInternalUri);
+            }else {
+                // Notify user that an error occurred while downloading image
+               // Snackbar.make(geta,"Error",Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // Custom method to convert string to url
+    protected URL stringToURL(String urlString){
+        try{
+            URL url = new URL(urlString);
+            return url;
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // Custom method to save a bitmap into internal storage
+    protected Uri saveImageToInternalStorage(Bitmap bitmap){
+        // Initialize ContextWrapper
+        //ContextWrapper wrapper = new ContextWrapper(getActivity());
+
+        // Initializing a new file
+        // The bellow line return a directory in internal storage
+       // File file = wrapper.getDir("Images",MODE_PRIVATE);
+
+        String root = Environment.getDataDirectory().toString();
+        File file = new File(root + "/yourDirectory");
+
+//        File file = new File(getActivity().getFilesDir(),"mydir");
+
+        // Create a file to save the image
+        file = new File(file, "UniqueFileName"+".jpg");
+
+        try{
+            // Initialize a new OutputStream
+            OutputStream stream = null;
+
+            // If the output file exists, it can be replaced or appended to it
+            stream = new FileOutputStream(file);
+
+            // Compress the bitmap
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+
+            // Flushes the stream
+            stream.flush();
+
+            // Closes the stream
+            stream.close();
+
+        }catch (IOException e) // Catch the exception
+        {
+            e.printStackTrace();
+        }
+
+        // Parse the gallery image url to uri
+        Uri savedImageURI = Uri.parse(file.getAbsolutePath());
+
+        // Return the saved image Uri
+        return savedImageURI;
+    }
 }
+
+
